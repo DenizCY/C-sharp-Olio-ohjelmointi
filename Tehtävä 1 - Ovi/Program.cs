ebcb528f52e3e554708126f1af4e8d4c1e3e9cf8
Deniz <s2100951@edu.bc.fi>
2023-02-09 12:08:23 +0200

    Lisätty ovi tehtävä

---
+++ Tehtävä 1 - Ovi/Program.cs
@@ -0,0 +1,9 @@
+﻿
+while (true)
+{
+    DoorState doorCurrentState = DoorState.Locked;
+    Console.Write($"The Door is {doorCurrentState}. What is your action? ");
+
+}
+enum DoorAction { Unlock, Open, Close, Lock };
+enum DoorState { Open, Closed, Locked };
